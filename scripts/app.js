e18e9c43358e6b589343291613278e9628a09726
parveen-sishodiya-bitbucket <parveen.sishodiya.official@gmail.com>
2021-02-13 11:26:43 +0530

    initial commit

---
+++ scripts/app.js
@@ -0,0 +1,20 @@
+console.log("I am home grocer");
+const packageName = require('mongo');
+
+let app = angular.module("shopper",["ngRoute"]);
+
+app.config($routeProvider => {
+    $routeProvider.when("/",{
+        templateUrl:"assets/views/dynamic/dashboard.html"
+    });
+    $routeProvider.when("/register",{
+        templateUrl:"assets/views/static/register.html",
+        controller:"registrationController"
+    });
+});
+
+app.controller("registrationController",function ($scope) {
+       $scope.registerUser = ()=>{
+           console.log($scope.user);
+       }
+})
